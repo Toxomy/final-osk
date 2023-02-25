43ecd4eacbc2862c8378fe32025dd8eb2c129392
Toxomy <96530062+Toxomy@users.noreply.github.com>
2023-02-25 15:00:51 +0600

    Update Kernel module.c

--- Kernel module.c
+++ Kernel module.c
@@ -3,17 +3,17 @@
 
 MODULE_LICENSE("GPL")
 MODULE_AUTHOR("toxomy")
-MODULE_DESCRIPTION("Hello World");
+MODULE_DESCRIPTION("Hello Aitu");
 
 static int __init hello_init(void)
 {
-  printk(KERN_INFO "Hello World!\n");
+  printk(KERN_INFO "Hello Aitu!\n");
   return 0;
 }
 
 static void __exit hello_exit(void)
 {
-  print(KERN_INFO "Goodbye!\n");
+  print(KERN_INFO "bye!\n");
 }
 
 module_init(hello_init);
